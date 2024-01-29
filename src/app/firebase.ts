db1435747a97c2d51e3f235f3e4f670c108e31bf
rahul-ar99 <rahulararukattu@gmail.com>
2024-01-29 17:46:08 +0530

    adding firebase api

---
+++ src/app/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDZc-QCVgtAQihWQNO3Fj7PCHOeIbjhdus",
+  authDomain: "netflix-clone-94a07.firebaseapp.com",
+  projectId: "netflix-clone-94a07",
+  storageBucket: "netflix-clone-94a07.appspot.com",
+  messagingSenderId: "1050918643201",
+  appId: "1:1050918643201:web:7e65eb90c8b7f3a69d4200"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
